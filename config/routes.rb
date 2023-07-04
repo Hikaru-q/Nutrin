3bef190a646fdbd4a86042625bad25eb00194bb4
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-07-04 08:24:28 +0000

    必須機能のcontroller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
   
+  # 管理者側のルーティング設定
+  namespace :admin do
+    root to: 'homes#top'
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+  end  
   
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about'
+    resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :customers, only:[:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end  
+  end  
   
   # 顧客用
   # URL /customers/sign_in ...
