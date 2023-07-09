2b4a9b92a331df1bfb0362a883a8a05c342bc1da
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-07-09 16:46:13 +0000

    管理者側からユーザー情報編集機能実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,31 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+    @posts = @customer.posts
+    @following_customers = @customer.following_customers
+    @follower_customers = @customer.follower_customers
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
+    flash[:notice] = "編集に成功しました"
+  end
+
+   private
+
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :email, :profile_image, :is_deleted)
+  end
+
 end
