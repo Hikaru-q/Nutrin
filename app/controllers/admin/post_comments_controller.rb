a3e12055c7a1a9a2262f8f06237c22be7be91e0e
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-07-12 15:24:58 +0000

    コメント機能実装

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,2 +1,15 @@
 class Admin::PostCommentsController < ApplicationController
+  
+  def destroy
+    @post_comment = PostComment.find(params[:id]).destroy
+    @post_comment.destroy
+    @post = Post.find(params[:post_id])
+  end
+  
+   private
+  
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+  
 end
